eab9eb35d40af78d56d6e9c77a6fd682f9c3add7
Tdecaz <tadas.kaluina@gmail.com>
2020-10-18 15:30:57 +0300

    2nd iteration: User roles added (ENUM)

--- src/main/java/com/artkls/articles/models/User.java
+++ src/main/java/com/artkls/articles/models/User.java
@@ -1,7 +1,14 @@
 package com.artkls.articles.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
@@ -14,10 +21,21 @@ public class User extends BaseEntity {
     private String username;
 
     @Column
+    @NotBlank
+    @Length(min=4)
+    @JsonIgnore
     private String password;
 
-    @Column
-    private String email;
 
-
+    @ManyToMany(
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL
+    )
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    @JsonManagedReference
+    private Set<Role> roles = new HashSet<>();
 }
